0a583af9a35e8f5f27fe8568aeaa35a9dd18b781
niinomi_ryota <niinomi_ryota@cyberagent.co.jp>
2015-02-08 13:48:11 +0900

    Add animation

--- js/myScript.js
+++ js/myScript.js
@@ -1,16 +1,29 @@
-var mousePos,
-    mouseDelta;
+var mousePoint = view.center;
+var amount = 25;
+var colors = ['pink', 'purple', 'blue', 'red'];
 
-function onMouseMove(event) {
-      mousePos = event.middlePoint;
-      mouseDelta = event.delta.length / 2;
+for (var i = 0; i < amount; i++) {
+    var rect = new Rectangle([0, 0], [25, 25]);
+    rect.center = mousePoint;
+    var path = new Path.Rectangle(rect, 11);
+    path.fillColor = colors[i % 4];
+    var scale = (1 - i / amount) * 20;
+    path.scale(scale);
 }
 
+function onMouseMove(event) {
+    mousePoint = event.point;
+}
+
+var children = project.activeLayer.children;
 function onFrame(event) {
-    var circle = new Path.Circle({
-        center : mousePos,
-        radius : mouseDelta
-    });
-    circle.strokeColor = "black";
-    circle.fillColoe   = "white";
+    for (var i = 0, l = children.length; i < l; i++) {
+        var item = children[i];
+        var delta = (mousePoint - item.position) / (i + 5);
+        var velocity = Math.sin(Math.floor(event.time * 10)) > 0 ? 1.02 : 0.98;
+        item.rotate(Math.sin((event.count + i) / 10) * 7);
+        item.scale(velocity);
+        if (delta.length > 0.1)
+            item.position += delta;
+    }
 }
